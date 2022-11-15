d5379c3b80c0227b6102c6fe2a299906802ceb0d
n.konohova <n.v.konokhova@gmail.com>
2022-11-15 14:57:39 +0300

    Setup gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ChakraProvider } from '@chakra-ui/react';
-import { initReactI18next } from 'react-i18next';
-import languageDetector from 'i18next-browser-languagedetector';
-import i18n from 'i18next';
 
 import App from './App';
 import { store } from './app/store';
@@ -17,9 +14,9 @@ root.render(
   /* <React.StrictMode> */
   <ChakraProvider>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </ChakraProvider>,
   /* </React.StrictMode> */
